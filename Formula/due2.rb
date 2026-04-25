96fd6963837ae346975b34614e9ae868bab67b9f
sheldon-myiam <sheldon.myiam@gmail.com>
2026-04-25 16:01:46 +0900

    due2: use homebrew-tap releases for binary downloads

--- Formula/due2.rb
+++ Formula/due2.rb
@@ -6,14 +6,14 @@ class Due2 < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/my-due2/due2-cli/releases/download/v#{version}/due2-#{version}-macos-arm64.tar.gz"
+      url "https://github.com/myiam-io/homebrew-tap/releases/download/due2-v#{version}/due2-#{version}-macos-arm64.tar.gz"
       sha256 "2a3b220e43de6da6d6127d90f79dddbc31649cd0d53dd6f303ba4e7b474f49d3"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/my-due2/due2-cli/releases/download/v#{version}/due2-#{version}-linux-x64.tar.gz"
+      url "https://github.com/myiam-io/homebrew-tap/releases/download/due2-v#{version}/due2-#{version}-linux-x64.tar.gz"
       sha256 "fcde029a15c70df5bafddb3920b45989cabad50c99ee79b2ffcfa629aa3376b9"
     end
   end
