13f3730b3164f907877a8588d665b5f339f3c076
sheldon-myiam <sheldon.myiam@gmail.com>
2026-04-25 16:13:37 +0900

    Rename due2 formula to due2-cli

---
+++ Formula/due2-cli.rb
@@ -0,0 +1,28 @@
+class Due2Cli < Formula
+  desc "CLI tool for managing due items with end-to-end encryption"
+  homepage "https://github.com/my-due2/due2-cli"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/myiam-io/homebrew-tap/releases/download/due2-cli-v#{version}/due2-cli-#{version}-macos-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/myiam-io/homebrew-tap/releases/download/due2-cli-v#{version}/due2-cli-#{version}-linux-x64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "due2-cli"
+  end
+
+  test do
+    assert_match "due2-cli", shell_output("#{bin}/due2-cli --help")
+  end
+end
